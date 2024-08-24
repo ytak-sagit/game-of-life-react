f047334d70f2a869c2f977e8b7ee6a5493787f02
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-08-24 20:35:25 +0900

    feat: シャーレ内にセルを描画できるように設定

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Cell } from "./Cell";
+import { Schale } from "./Schale";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -25,11 +27,16 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <Schale>
+        {[...Array(200)].map((_, i) => (
+          <Cell key={i} alive={Math.random() >= 0.5} />
+        ))}
+      </Schale>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
