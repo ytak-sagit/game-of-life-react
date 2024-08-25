155af58648a444876deb9462cfb78a2ab7f02768
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-08-25 22:45:31 +0900

    style: 不要な要素を削除

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Cell } from "./Cell";
 import { Schale } from "./Schale";
@@ -34,26 +32,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={onClick}>
-          Next generation
-        </button>
+      <button type="button" onClick={onClick}>
+        Next generation
+      </button>
       <p>Generaion is #{generation}</p>
       <Schale width={width * 10} height={height * 10}>
         {cells}
       </Schale>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
