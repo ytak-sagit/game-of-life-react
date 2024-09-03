0253fb3a6bd46690547387570a9cd0bf9eb9f3c4
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-09-03 23:14:36 +0900

    perf: 次世代判定ロジックを最適化 - `Array.map()`から`for`文へ書き換えた

--- app/src/gol-rule.ts
+++ app/src/gol-rule.ts
@@ -8,34 +8,38 @@ export const apply = (
   width: number,
   height: number,
 ) => {
+  const nextAliveStates = Array<aliveState>(aliveStates.length).fill(DEAD);
   const widthWithOutside = width + 2;
-  const heightWithOutside = height + 2;
 
-  return aliveStates.map<aliveState>((state, index, states) => {
-    if (isOutside(index, widthWithOutside, heightWithOutside)) {
-      return DEAD;
+  for (let y = 1; y <= height; y++) {
+    for (let x = 1; x <= width; x++) {
+      const offset = 1 + y * widthWithOutside + (x - 1);
+
+      const upLeft = aliveStates[offset - (widthWithOutside + 1)];
+      const up = aliveStates[offset - widthWithOutside];
+      const upRight = aliveStates[offset - (widthWithOutside - 1)];
+      const left = aliveStates[offset - 1];
+      const central = aliveStates[offset];
+      const right = aliveStates[offset + 1];
+      const downLeft = aliveStates[offset + (widthWithOutside - 1)];
+      const down = aliveStates[offset + widthWithOutside];
+      const downRight = aliveStates[offset + (widthWithOutside + 1)];
+
+      const sumAliveCells =
+        upLeft + up + upRight + left + right + downLeft + down + downRight;
+
+      // 誕生or生存のパターンに該当すれば次世代は生存
+      // 過疎or過密orそれ以外のパターンであれば次世代は死亡
+
+      // 誕生: 現在の状態=死亡, 近傍の生存セル数=3
+      // 生存: 現在の状態=生存, 近傍の生存セル数=2or3
+      // よって、現在の状態と近傍の生存セル数のORをとった値がちょうど3であれば、次世代は生存となる
+      nextAliveStates[offset] =
+        (central | sumAliveCells) === 0b0011 ? ALIVE : DEAD;
     }
+  }
 
-    const upLeft = states[index - (widthWithOutside + 1)];
-    const up = states[index - widthWithOutside];
-    const upRight = states[index - (widthWithOutside - 1)];
-    const left = states[index - 1];
-    const right = states[index + 1];
-    const downLeft = states[index + (widthWithOutside - 1)];
-    const down = states[index + widthWithOutside];
-    const downRight = states[index + (widthWithOutside + 1)];
-
-    const sumAliveCells =
-      upLeft + up + upRight + left + right + downLeft + down + downRight;
-
-    // 誕生or生存のパターンに該当すれば次世代は生存
-    // 過疎or過密orそれ以外のパターンであれば次世代は死亡
-
-    // 誕生: 現在の状態=死亡, 近傍の生存セル数=3
-    // 生存: 現在の状態=生存, 近傍の生存セル数=2or3
-    // よって、現在の状態と近傍の生存セル数のORをとった値がちょうど3であれば、次世代は生存となる
-    return (state | sumAliveCells) === 0b0011 ? ALIVE : DEAD;
-  });
+  return nextAliveStates;
 };
 
 export const isOutside = (
