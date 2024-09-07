f6212356efe0ec2d8e2b0432a4d4eb6898c09337
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-09-07 18:29:50 +0900

    style: `Schale`コンポーネントを`features`ディレクトリ以下へ移動

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,7 +7,7 @@ import {
   NUMBER_OF_CELLS_PER_ROW,
 } from "~/config/environments";
 import { Cell } from "~/features/cell/cell";
-import { Schale } from "~/Schale";
+import { Schale } from "~/features/schale/schale";
 import { ALIVE, apply, DEAD, type aliveState } from "~/utils/gol-rule";
 
 function App() {
