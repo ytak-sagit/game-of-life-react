b4bc685c813cc428e06f7c74cbef98c34587bb74
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-09-07 17:48:18 +0900

    style: add biome ignore lint

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "~/App.tsx";
 import "~/index.css";
 
+// biome-ignore lint: non-null assertion should be allowed
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
