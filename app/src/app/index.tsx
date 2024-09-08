9c56acfb697ff0c4c90c49cd8c728229d6366a63
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-09-08 18:25:38 +0900

    refactor: replace to hooks

--- app/src/app/index.tsx
+++ app/src/app/index.tsx
@@ -1,4 +1,3 @@
-import { useRef, useState } from "react";
 import {
   CELL_HEIGHT,
   CELL_WIDTH,
@@ -7,78 +6,43 @@ import {
 } from "~/config/environments";
 import { Cell } from "~/features/cell";
 import { Schale } from "~/features/schale";
-import { random } from "~/utils/gol-pattern-generator";
-import { ALIVE, apply, DEAD, type aliveState } from "~/utils/gol-rule";
+import { useCellStates } from "~/hooks/use-cell-states";
+import { usePolling } from "~/hooks/use-polling";
+import { ALIVE } from "~/utils/gol-rule";
 
 export const App = () => {
-  const [aliveState, setAliveState] = useState(
-    [
-      ...Array(NUMBER_OF_CELLS_PER_ROW * NUMBER_OF_CELLS_PER_COL),
-    ].map<aliveState>(random),
-  );
-  const [generation, setGeneration] = useState(0);
+  const {
+    cellStates,
+    generation,
+    incrementCellStates,
+    resetCellStates,
+    toggleCellStateAt,
+  } = useCellStates();
+  const { isPolling, togglePolling } = usePolling(incrementCellStates, 100);
 
-  const onClick = () => {
-    setAliveState((prev) => [
-      ...apply(prev, NUMBER_OF_CELLS_PER_ROW, NUMBER_OF_CELLS_PER_COL),
-    ]);
-    setGeneration((prev) => prev + 1);
-  };
-
-  const [isSimulating, setIsSimulating] = useState(false);
-  const simulatingRef = useRef(isSimulating);
-  simulatingRef.current = isSimulating;
-  // TODO: useCallback() の導入検討
-  const simulating = () => {
-    if (!simulatingRef.current) {
-      return;
-    }
-
-    onClick();
-
-    // 再帰呼び出し
-    setTimeout(simulating, 100);
-  };
-
-  const onClickCell = (index: number) => () => {
-    aliveState[index] ^= ALIVE;
-    setAliveState([...aliveState]);
-  };
-
-  const cells = aliveState.map((alive, i) => {
+  const cells = cellStates.map((state, i) => {
     const key = i;
     return (
-      <Cell key={key} alive={alive === ALIVE} onClick={onClickCell(key)} />
+      <Cell
+        key={key}
+        alive={state === ALIVE}
+        onClick={toggleCellStateAt(key)}
+      />
     );
   });
 
-  const onReset = () => {
-    setAliveState(Array<aliveState>(aliveState.length).fill(DEAD));
-    setGeneration(0);
-  };
+  const onClickStartOrStop = () => togglePolling();
+  const onClickNext = () => incrementCellStates();
+  const onReset = () => resetCellStates();
 
   // TODO: Start/Stopボタンのコンポーネント化
   // TODO: Startボタン活性時、Next generationボタンを非活性にする
   return (
     <>
-      <button
-        type="button"
-        onClick={() => {
-          setIsSimulating((prev) => !prev);
-          // NOTE:
-          // setState() は非同期で state を更新するため、
-          // setState() 直後は更新前の state の値が参照される
-          if (isSimulating) {
-            simulatingRef.current = false;
-          } else {
-            simulatingRef.current = true;
-            simulating();
-          }
-        }}
-      >
-        {isSimulating ? "Stop" : "Start"}
+      <button type="button" onClick={onClickStartOrStop}>
+        {isPolling ? "Stop" : "Start"}
       </button>
-      <button type="button" onClick={onClick}>
+      <button type="button" onClick={onClickNext}>
         Next
       </button>
       <button type="reset" onClick={onReset}>
