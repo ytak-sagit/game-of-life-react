b0a8e09051351980eb563819350b86154753a825
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-09-23 23:14:55 +0900

    feat: ブラウザの横幅と縦幅に応じて、セル配置件数を動的に変更できるように対応

--- app/src/config/environments.ts
+++ app/src/config/environments.ts
@@ -6,8 +6,20 @@ export const CELL_WIDTH = 10;
 /** セルの縦幅 */
 export const CELL_HEIGHT = 10;
 
+/** シャーレ1行あたりのセル配置件数 既定値 */
+const DEFAULT_NUMBER_OF_CELLS_PER_ROW = 128;
+
 /** シャーレ1行あたりのセル配置件数 */
-export const NUMBER_OF_CELLS_PER_ROW = 80;
+export const NUMBER_OF_CELLS_PER_ROW = Math.min(
+  DEFAULT_NUMBER_OF_CELLS_PER_ROW,
+  Math.trunc(window.innerWidth / CELL_WIDTH) - 4,
+);
+
+/** シャーレ1列あたりのセル配置件数 既定値 */
+const DEFAULT_NUMBER_OF_CELLS_PER_COL = 98;
 
 /** シャーレ1列あたりのセル配置件数 */
-export const NUMBER_OF_CELLS_PER_COL = 80;
+export const NUMBER_OF_CELLS_PER_COL = Math.min(
+  DEFAULT_NUMBER_OF_CELLS_PER_COL,
+  Math.trunc(window.innerHeight / CELL_HEIGHT) - 14,
+);
