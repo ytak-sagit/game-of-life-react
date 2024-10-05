48792be06de07da0bbcc0c024b139a258fd1bfef
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-10-05 23:14:08 +0900

    style: 定数をaliasで参照するように変更

--- app/src/utils/gol-pattern-generator.ts
+++ app/src/utils/gol-pattern-generator.ts
@@ -1,4 +1,3 @@
-import { ALIVE, type CellAliveState, DEAD } from "./gol-rule";
+import { ALIVE as A, type CellAliveState, DEAD as D } from "./gol-rule";
 
-export const random = (): CellAliveState =>
-  Math.random() >= 0.5 ? ALIVE : DEAD;
+export const random = (): CellAliveState => (Math.random() >= 0.5 ? A : D);
