9d60214d73635398f84356e2be75129daedac446
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-11-03 01:46:12 +0900

    feat: 並びの方向を指定できるように対応

--- app/src/components/flex-list/index.tsx
+++ app/src/components/flex-list/index.tsx
@@ -3,6 +3,8 @@ import { css } from "@emotion/react";
 import type { ReactElement } from "react";
 
 type FlexListProps = {
+  /** 子コンポーネントの並びの方向("column":縦, "row":横) */
+  flexDirection?: "column" | "row";
   /** 子コンポーネント間の余白サイズ(pixel) */
   gapSizePx?: number;
   /** 子コンポーネント(複数指定が必須) */
@@ -10,12 +12,14 @@ type FlexListProps = {
 };
 
 export const FlexList: React.FC<FlexListProps> = ({
+  flexDirection = "row",
   gapSizePx = 12,
   children,
 }) => {
   const ulStyle = css({
     display: "flex",
     justifyContent: "center",
+    flexDirection,
     columnGap: `${gapSizePx}px`,
     listStyle: "none",
     paddingInlineStart: "unset",
