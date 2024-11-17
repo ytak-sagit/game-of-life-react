bb62902bec6f7c0c03246da83f21d3a0614bcd95
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-11-17 22:07:00 +0900

    refactor: 各種パターンを`index.ts`から参照できるように修正

--- app/src/utils/gol-pattern-store.ts
+++ app/src/utils/gol-pattern-store.ts
@@ -1,14 +1,16 @@
-import { clock01 } from "./gol-patterns/clock-01";
-import { clock02 } from "./gol-patterns/clock-02";
-import { glider } from "./gol-patterns/glider";
-import { gliderGun } from "./gol-patterns/glider-gun";
-import { heavyweightSpaceship } from "./gol-patterns/heavyweight-spaceship";
-import { lightweightSpaceship } from "./gol-patterns/lightweight-spaceship";
-import { middleweightSpaceship } from "./gol-patterns/middleweight-spaceship";
-import { nebula } from "./gol-patterns/nebula";
-import { pufferTrain } from "./gol-patterns/puffer-train";
-import { pulsar } from "./gol-patterns/pulsar";
-import { random } from "./gol-patterns/random";
+import {
+  clock01,
+  clock02,
+  glider,
+  gliderGun,
+  heavyweightSpaceship,
+  lightweightSpaceship,
+  middleweightSpaceship,
+  nebula,
+  pufferTrain,
+  pulsar,
+  random,
+} from "./gol-patterns";
 
 export const patternStore = {
   random,
