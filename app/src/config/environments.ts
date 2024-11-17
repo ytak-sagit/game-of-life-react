66041257c022dcde0778ae814e34b577b66ffd22
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-11-17 23:40:24 +0900

    fix: 環境変数が`string`のまま扱われていたので、明示的に`number`へ変換するよう修正

--- app/src/config/environments.ts
+++ app/src/config/environments.ts
@@ -1,13 +1,15 @@
 /** セルの横幅 */
-export const CELL_WIDTH = import.meta.env.VITE_CELL_WIDTH;
+export const CELL_WIDTH = Number.parseInt(import.meta.env.VITE_CELL_WIDTH);
 
 /** セルの縦幅 */
-export const CELL_HEIGHT = import.meta.env.VITE_CELL_HEIGHT;
+export const CELL_HEIGHT = Number.parseInt(import.meta.env.VITE_CELL_HEIGHT);
 
 /** シャーレ1行あたりのセル配置件数 */
-export const NUMBER_OF_CELLS_PER_ROW = import.meta.env
-  .VITE_NUMBER_OF_CELLS_PER_ROW;
+export const NUMBER_OF_CELLS_PER_ROW = Number.parseInt(
+  import.meta.env.VITE_NUMBER_OF_CELLS_PER_ROW,
+);
 
 /** シャーレ1列あたりのセル配置件数 */
-export const NUMBER_OF_CELLS_PER_COL = import.meta.env
-  .VITE_NUMBER_OF_CELLS_PER_COL;
+export const NUMBER_OF_CELLS_PER_COL = Number.parseInt(
+  import.meta.env.VITE_NUMBER_OF_CELLS_PER_COL,
+);
--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_CELL_WIDTH: number;
-  readonly VITE_CELL_HEIGHT: number;
-  readonly VITE_NUMBER_OF_CELLS_PER_ROW: number;
-  readonly VITE_NUMBER_OF_CELLS_PER_COL: number;
+  readonly VITE_CELL_WIDTH: string;
+  readonly VITE_CELL_HEIGHT: string;
+  readonly VITE_NUMBER_OF_CELLS_PER_ROW: string;
+  readonly VITE_NUMBER_OF_CELLS_PER_COL: string;
 }
 
 interface ImportMeta {
