1e7fe43ab79c88c3844cece8203a61d2e8f2e596
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-11-17 19:23:34 +0900

    refactor: セルのサイズと配置件数を環境変数として定義できるように対応

--- app/src/config/environments.ts
+++ app/src/config/environments.ts
@@ -1,13 +1,13 @@
-// TODO: 環境変数として設定できるようにする
-
 /** セルの横幅 */
-export const CELL_WIDTH = 10;
+export const CELL_WIDTH = import.meta.env.VITE_CELL_WIDTH;
 
 /** セルの縦幅 */
-export const CELL_HEIGHT = 10;
+export const CELL_HEIGHT = import.meta.env.VITE_CELL_HEIGHT;
 
 /** シャーレ1行あたりのセル配置件数 */
-export const NUMBER_OF_CELLS_PER_ROW = 128;
+export const NUMBER_OF_CELLS_PER_ROW = import.meta.env
+  .VITE_NUMBER_OF_CELLS_PER_ROW;
 
 /** シャーレ1列あたりのセル配置件数 */
-export const NUMBER_OF_CELLS_PER_COL = 128;
+export const NUMBER_OF_CELLS_PER_COL = import.meta.env
+  .VITE_NUMBER_OF_CELLS_PER_COL;
--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_CELL_WIDTH: number;
+  readonly VITE_CELL_HEIGHT: number;
+  readonly VITE_NUMBER_OF_CELLS_PER_ROW: number;
+  readonly VITE_NUMBER_OF_CELLS_PER_COL: number;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
